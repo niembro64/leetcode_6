45261c12d9b584646da0c7b3327ef15f468693f2
niemoBroIntegration <49417819+niemoBroIntegration@users.noreply.github.com>
2022-07-14 00:42:43 -0400

    init

---
+++ leetcode_7.ts
@@ -0,0 +1,87 @@
+// 2 -> every 2
+// 3 -> every 4
+// 4 -> every 6
+// 5 -> every 8
+// PAYPALISHIRING
+//
+// P   H
+// A  SI
+// Y I R
+// PL  IG
+// A   N
+
+function convert(s: string, numRows: number): string {
+    if (numRows === 1) {
+        return s;
+    }
+    var numArrays: number = (numRows - 1) * 2;
+    var f: string = "";
+
+    for (let i = 0; i < numRows; i++) {
+        if (i === 0 || i === numRows - 1) {
+            for (let j = i; j < s.length; j += numArrays) {
+                f += s[j];
+                // console.log(j);
+            }
+        } else {
+            for (let j = i; j < s.length + numArrays; j += numArrays) {
+                f += s[j - i * 2] && j - i * 2 >= 0 ? s[j - i * 2] : "";
+                // console.log(j - i * 2);
+                f += s[j] ? s[j] : "";
+                // console.log(j);
+            }
+        }
+    }
+
+    return f;
+}
+
+var a = [
+    { s: "PAYPALISHIRING", n: 3 },
+    { s: "PAYPALISHIRING", n: 4 },
+    { s: "A", n: 1 },
+];
+
+a.forEach((a, i) => {
+    console.log(a.n, a.s, convert(a.s, a.n));
+    console.log("____");
+});
+
+// function convert(s: string, numRows: number): string {
+//     if (numRows === 1) {
+//         return s;
+//     }
+//     // each array has (n-1)*2 elements
+//     var x: [string[]] = [[]];
+//     var numArrays = (numRows - 1) * 2;
+//     var currArray: number = 0;
+//     var finalString: string = "";
+
+//     // prepare arrays
+//     for (let i = 0; i < (numRows - 1) * 2; i++) {
+//         x.push([]);
+//     }
+
+//     // populate arrays
+//     for (var i = 0; i < s.length; i++) {
+//         x[Math.floor(i / numArrays)].push(s[i]);
+//     }
+
+//     // print
+//     var j = 0;
+//     for (let i = 0; i < numArrays; i++) {
+//         j = 0;
+//         while (x[i][j]) {
+//             console.log(i, j, x[i][j]);
+//             j++;
+//         }
+//     }
+
+//     for (let i = 0; i < s.length; i++) {
+//         if (i % numArrays <= numArrays / 2) {
+//             finalString+=
+//         }
+//     }
+
+//     return finalString;
+// }
